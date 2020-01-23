61fca0be20c95df725b105274747adfcf23f5829
eating-coleslaw <10554267+eating-coleslaw@users.noreply.github.com>
2020-01-23 11:57:15 -0600

    add empty MatchConfiguration class

---
+++ squittal.ScrimPlanetmans.Shared/Models/ScrimEngine/MatchConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace squittal.ScrimPlanetmans.Shared.Models.ScrimEngine
+{
+    public class MatchConfiguration
+    {
+    }
+}
