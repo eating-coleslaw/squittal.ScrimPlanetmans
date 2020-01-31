24840c429e1fd75b32e9503a754de70c403a53ea
eating-coleslaw <10554267+eating-coleslaw@users.noreply.github.com>
2020-01-31 12:07:39 -0600

    add online status stuff

---
+++ squittal.ScrimPlanetmans.App/CensusServices/Models/CensusOutfitMemberCharacterModel.cs
@@ -0,0 +1,10 @@
+﻿using static squittal.ScrimPlanetmans.CensusServices.Models.CensusCharacterModel;
+
+namespace squittal.ScrimPlanetmans.CensusServices.Models
+{
+    public class CensusOutfitMemberCharacterModel : CensusOutfitMemberModel
+    {
+        public CharacterName Name { get; set; }
+        public int OnlineStatus { get; set; }
+    }
+}
---
+++ squittal.ScrimPlanetmans.App/CensusServices/Models/CensusOutfitResolveMemberCharacterModel.cs
@@ -0,0 +1,10 @@
+﻿using squittal.ScrimPlanetmans.CensusServices.Models;
+using System.Collections.Generic;
+
+namespace squittal.ScrimPlanetmans.CensusServices.Models
+{
+    public class CensusOutfitResolveMemberCharacterModel : CensusOutfitModel
+    {
+        public IEnumerable<CensusOutfitMemberCharacterModel> Members { get; set; }
+    }
+}
