d82cc1aefc5915c34f57465302418645bb982614
Chirtle <10554267+chirtle@users.noreply.github.com>
2020-02-21 11:45:12 -0600

    Add SqCssHelper class

---
+++ squittal.ScrimPlanetmans.App/Models/SqCssHelper.cs
@@ -0,0 +1,30 @@
+﻿namespace squittal.ScrimPlanetmans.Models
+{
+    public static class SqCssHelper
+    {
+        public static string GetFactionClassFromId(int? factionId)
+        {
+            var cssClass = factionId switch
+            {
+                1 => "vs",
+                2 => "nc",
+                3 => "tr",
+                4 => "ns",
+                _ => "ns",
+            };
+            return cssClass;
+        }
+
+        public static string GetZoneDisplayEmojiFromName(string zoneName)
+        {
+            return zoneName switch
+            {
+                "Amerish" => "🗻",
+                "Esamir" => "❄️",
+                "Hossin" => "🌳",
+                "Indar" => "☀️",
+                _ => "❔",
+            };
+        }
+    }
+}
