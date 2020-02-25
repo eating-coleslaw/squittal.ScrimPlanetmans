e9bf8659f6a67d0b1534f3deeece66eda98a3492
Chirtle <10554267+chirtle@users.noreply.github.com>
2020-02-25 11:36:56 -0600

    actually use DbInitializer in Program.cs, remove unnecessary usings

--- squittal.ScrimPlanetmans.App/Program.cs
+++ squittal.ScrimPlanetmans.App/Program.cs
@@ -1,13 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using squittal.ScrimPlanetmans.Data;
 
 namespace squittal.ScrimPlanetmans.App
 {
@@ -15,7 +11,26 @@ namespace squittal.ScrimPlanetmans.App
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //CreateHostBuilder(args).Build().Run();
+
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    DbInitializer.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occured initializing the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
