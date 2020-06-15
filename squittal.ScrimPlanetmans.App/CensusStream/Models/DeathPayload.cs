c0f7ebf224eb6a8c6ab1760bf03d633ebc403a56
Chirtle <10554267+chirtle@users.noreply.github.com>
2020-06-15 10:03:05 -0500

    implement IEquitable in event payload models

--- squittal.ScrimPlanetmans.App/CensusStream/Models/DeathPayload.cs
+++ squittal.ScrimPlanetmans.App/CensusStream/Models/DeathPayload.cs
@@ -1,6 +1,9 @@
-﻿namespace squittal.ScrimPlanetmans.CensusStream.Models
+﻿using squittal.ScrimPlanetmans.Models.Planetside;
+using squittal.ScrimPlanetmans.ScrimMatch.Models;
+
+namespace squittal.ScrimPlanetmans.CensusStream.Models
 {
-    public class DeathPayload : PayloadBase
+    public class DeathPayload : PayloadBase, IEquitable<DeathPayload>
     {
         public string AttackerCharacterId { get; set; }
         public int? AttackerFireModeId { get; set; }
@@ -10,5 +13,59 @@
         public string CharacterId { get; set; }
         public int? CharacterLoadoutId { get; set; }
         public bool IsHeadshot { get; set; }
+
+        #region IEquitable
+        public override bool Equals(object obj)
+        {
+            return this.Equals(obj as DeathPayload);
+        }
+
+        public bool Equals(DeathPayload p)
+        {
+            if (ReferenceEquals(p, null))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, p))
+            {
+                return true;
+            }
+
+            if (this.GetType() != p.GetType())
+            {
+                return false;
+            }
+
+            return (p.Timestamp == Timestamp
+                    && p.AttackerCharacterId == AttackerCharacterId
+                    && p.CharacterId == CharacterId);
+        }
+
+        public static bool operator ==(DeathPayload lhs, DeathPayload rhs)
+        {
+            if (ReferenceEquals(lhs, null))
+            {
+                if (ReferenceEquals(rhs, null))
+                {
+                    return true;
+                }
+
+                return false;
+            }
+            return lhs.Equals(rhs);
+        }
+
+        public static bool operator !=(DeathPayload lhs, DeathPayload rhs)
+        {
+            return !(lhs == rhs);
+        }
+
+        public override int GetHashCode()
+        {
+            var id = $"t{Timestamp:yyyyMMddTHHmmss}a{AttackerCharacterId}v{CharacterId}";
+            return id.GetHashCode();
+        }
+        #endregion IEquitable
     }
 }
--- squittal.ScrimPlanetmans.App/CensusStream/Models/VehicleDestroyPayload.cs
+++ squittal.ScrimPlanetmans.App/CensusStream/Models/VehicleDestroyPayload.cs
@@ -1,6 +1,8 @@
-﻿namespace squittal.ScrimPlanetmans.CensusStream.Models
+﻿using squittal.ScrimPlanetmans.ScrimMatch.Models;
+
+namespace squittal.ScrimPlanetmans.CensusStream.Models
 {
-    public class VehicleDestroyPayload : PayloadBase
+    public class VehicleDestroyPayload : PayloadBase, IEquitable<VehicleDestroyPayload>
     {
         public string AttackerCharacterId { get; set; }
         public int? AttackerLoadoutId { get; set; }
@@ -10,5 +12,60 @@
         public int VehicleId { get; set; }
         public int? FactionId { get; set; }
         public int? FacilityId { get; set; }
+
+        #region IEquitable
+        public override bool Equals(object obj)
+        {
+            return this.Equals(obj as VehicleDestroyPayload);
+        }
+
+        public bool Equals(VehicleDestroyPayload p)
+        {
+            if (ReferenceEquals(p, null))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, p))
+            {
+                return true;
+            }
+
+            if (this.GetType() != p.GetType())
+            {
+                return false;
+            }
+
+            return (p.Timestamp == Timestamp
+                    && p.AttackerCharacterId == AttackerCharacterId
+                    && p.VehicleId == VehicleId
+                    && p.CharacterId == CharacterId);
+        }
+
+        public static bool operator ==(VehicleDestroyPayload lhs, VehicleDestroyPayload rhs)
+        {
+            if (ReferenceEquals(lhs, null))
+            {
+                if (ReferenceEquals(rhs, null))
+                {
+                    return true;
+                }
+
+                return false;
+            }
+            return lhs.Equals(rhs);
+        }
+
+        public static bool operator !=(VehicleDestroyPayload lhs, VehicleDestroyPayload rhs)
+        {
+            return !(lhs == rhs);
+        }
+
+        public override int GetHashCode()
+        {
+            var id = $"t{Timestamp:yyyyMMddTHHmmss}a{AttackerCharacterId}veh{VehicleId}v{CharacterId}";
+            return id.GetHashCode();
+        }
+        #endregion IEquitable
     }
 }
