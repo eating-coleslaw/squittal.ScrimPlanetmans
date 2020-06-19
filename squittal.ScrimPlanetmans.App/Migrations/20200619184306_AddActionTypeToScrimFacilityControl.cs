fcaa66ce32cf93fd8608d3387e69252638ae28fa
Chirtle <10554267+chirtle@users.noreply.github.com>
2020-06-19 14:21:45 -0500

    add migration for adding ActionType column ScrimFacilityControl table

---
+++ squittal.ScrimPlanetmans.App/Migrations/20200619184306_AddActionTypeToScrimFacilityControl.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace squittal.ScrimPlanetmans.App.Migrations
+{
+    public partial class AddActionTypeToScrimFacilityControl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ActionType",
+                table: "ScrimFacilityControl",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ActionType",
+                table: "ScrimFacilityControl");
+        }
+    }
+}
