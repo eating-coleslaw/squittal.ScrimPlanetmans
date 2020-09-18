022219e30fe1e93d988f65ab8503b45c16452202
Chirtle <10554267+chirtle@users.noreply.github.com>
2020-09-18 12:44:30 -0500

    default WeaponFactionId = 4 if null or 0

--- squittal.ScrimPlanetmans.App/Data/SQL/Views/View_ScrimMatchReportInfantryPlayerWeaponStats.sql
+++ squittal.ScrimPlanetmans.App/Data/SQL/Views/View_ScrimMatchReportInfantryPlayerWeaponStats.sql
@@ -19,7 +19,7 @@ ALTER VIEW View_ScrimMatchReportInfantryPlayerWeaponStats AS
          match_players.PrestigeLevel PrestigeLevel,
          player_weapon_combos.WeaponId WeaponId,
          weapons.Name WeaponName,
-         COALESCE( weapons.FactionId, 4 ) WeaponFactionId,
+         CASE WHEN COALESCE( weapons.FactionId, 0 ) = 0 THEN 4 ELSE weapons.FactionId END WeaponFactionId,
          COALESCE(kill_sums.Points, 0) Points,
          COALESCE(kill_sums.NetScore, 0) + COALESCE(death_sums.NetScore, 0) NetScore,
          COALESCE(kill_sums.Kills, 0) Kills,
