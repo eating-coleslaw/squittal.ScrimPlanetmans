a6077f44d0c2f50b21fd1e1b64b26f3111ca0e05
Chirtle <10554267+chirtle@users.noreply.github.com>
2020-10-15 11:01:42 -0500

    fix RulesetItemCategorRule changes not saving

--- squittal.ScrimPlanetmans.App/Services/Rulesets/RulesetDataService.cs
+++ squittal.ScrimPlanetmans.App/Services/Rulesets/RulesetDataService.cs
@@ -727,9 +727,9 @@ namespace squittal.ScrimPlanetmans.Services.Rulesets
                             storeEntity = rule;
                             //storeEntity.ItemCategory = null;
 
-                            //storeEntity.Points = rule.Points;
-                            //storeEntity.IsBanned = rule.IsBanned;
-                            //storeEntity.DeferToItemRules = rule.DeferToItemRules;
+                            storeEntity.Points = rule.Points;
+                            storeEntity.IsBanned = rule.IsBanned;
+                            storeEntity.DeferToItemRules = rule.DeferToItemRules;
 
                             dbContext.RulesetItemCategoryRules.Update(storeEntity);
                             //dbContext.RulesetItemCategoryRules.Update(rule);
@@ -741,19 +741,19 @@ namespace squittal.ScrimPlanetmans.Services.Rulesets
                         dbContext.RulesetItemCategoryRules.AddRange(newEntities);
                     }
 
-                    //var storeRuleset = await dbContext.Rulesets.Where(r => r.Id == rulesetId).FirstOrDefaultAsync();
+                    var storeRuleset = await dbContext.Rulesets.Where(r => r.Id == rulesetId).FirstOrDefaultAsync();
 
-                    ////var storeRuleset = await GetRulesetFromIdAsync(rulesetId, CancellationToken.None, false);
+                    //var storeRuleset = await GetRulesetFromIdAsync(rulesetId, CancellationToken.None, false);
 
-                    //if (storeRuleset != null)
-                    //{
-                    //    storeRuleset.DateLastModified = DateTime.UtcNow;
-                    //    dbContext.Rulesets.Update(storeRuleset);
-                    //}
+                    if (storeRuleset != null)
+                    {
+                        storeRuleset.DateLastModified = DateTime.UtcNow;
+                        dbContext.Rulesets.Update(storeRuleset);
+                    }
 
-                    //await dbContext.SaveChangesAsync();
+                    await dbContext.SaveChangesAsync();
 
-                    var storeRuleset = await UpdateRulesetDateLastModified(rulesetId, DateTime.UtcNow);
+                    //var storeRuleset = await UpdateRulesetDateLastModified(rulesetId, DateTime.UtcNow);
 
                     var message = new RulesetRuleChangeMessage(storeRuleset, RulesetRuleChangeType.ItemCategoryRule);
                     _messageService.BroadcastRulesetRuleChangeMessage(message);
