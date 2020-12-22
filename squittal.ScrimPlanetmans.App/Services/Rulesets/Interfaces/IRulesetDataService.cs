9d1d6319cc94315b6d35ab9c14e5a281be61c79d
Chirtle <10554267+chirtle@users.noreply.github.com>
2020-12-22 11:15:43 -0600

    organize methods

--- squittal.ScrimPlanetmans.App/Services/Rulesets/Interfaces/IRulesetDataService.cs
+++ squittal.ScrimPlanetmans.App/Services/Rulesets/Interfaces/IRulesetDataService.cs
@@ -16,32 +16,36 @@ namespace squittal.ScrimPlanetmans.Services.Rulesets
 
         Task<PaginatedList<Ruleset>> GetRulesetListAsync(int? pageIndex, CancellationToken cancellationToken);
         Task<Ruleset> GetRulesetFromIdAsync(int rulesetId, CancellationToken cancellationToken, bool includeCollections = true, bool includeOverlayConfiguration = true);
-        Task<IEnumerable<RulesetActionRule>> GetRulesetActionRulesAsync(int rulesetId, CancellationToken cancellationToken);
-        Task<IEnumerable<RulesetItemCategoryRule>> GetRulesetItemCategoryRulesAsync(int rulesetId, CancellationToken cancellationToken);
         Task<IEnumerable<Ruleset>> GetAllRulesetsAsync(CancellationToken cancellationToken);
+
+        Task<IEnumerable<RulesetActionRule>> GetRulesetActionRulesAsync(int rulesetId, CancellationToken cancellationToken);
+        Task<RulesetOverlayConfiguration> GetRulesetOverlayConfigurationAsync(int rulesetId, CancellationToken cancellationToken);
+        Task<IEnumerable<RulesetItemCategoryRule>> GetRulesetItemCategoryRulesAsync(int rulesetId, CancellationToken cancellationToken);
+        Task<IEnumerable<RulesetItemRule>> GetRulesetItemRulesAsync(int rulesetId, CancellationToken cancellationToken);
         Task<IEnumerable<RulesetFacilityRule>> GetRulesetFacilityRulesAsync(int rulesetId, CancellationToken cancellationToken);
         Task<IEnumerable<RulesetFacilityRule>> GetUnusedRulesetFacilityRulesAsync(int rulesetId, CancellationToken cancellationToken);
+
         Task<Ruleset> GetRulesetWithFacilityRules(int rulesetId, CancellationToken cancellationToken);
+        Task<IEnumerable<RulesetItemCategoryRule>> GetRulesetItemCategoryRulesDeferringToItemRules(int rulesetId, CancellationToken cancellationToken);
+        Task<IEnumerable<ItemCategory>> GetItemCategoriesDeferringToItemRules(int rulesetId, CancellationToken cancellationToken);
         
-        Task<bool> SaveRulesetOverlayConfiguration(int rulesetId, RulesetOverlayConfiguration rulesetOverlayConfiguration, CancellationToken cancellationToken);
-        Task SaveRulesetItemCategoryRules(int rulesetId, IEnumerable<RulesetItemCategoryRule> rules);
-        Task SaveRulesetActionRules(int rulesetId, IEnumerable<RulesetActionRule> rules);
         Task<Ruleset> SaveNewRulesetAsync(Ruleset ruleset);
-        Task SaveRulesetFacilityRules(int rulesetId, IEnumerable<RulesetFacilityRuleChange> rules);
         Task<bool> UpdateRulesetInfo(Ruleset rulesetUpdate, CancellationToken cancellationToken);
-        
+        Task<bool> SaveRulesetOverlayConfiguration(int rulesetId, RulesetOverlayConfiguration rulesetOverlayConfiguration, CancellationToken cancellationToken);
+        Task SaveRulesetActionRules(int rulesetId, IEnumerable<RulesetActionRule> rules);
+        Task SaveRulesetItemCategoryRules(int rulesetId, IEnumerable<RulesetItemCategoryRule> rules);
+        Task SaveRulesetItemRules(int rulesetId, IEnumerable<RulesetItemRule> rules);
+        Task SaveRulesetFacilityRules(int rulesetId, IEnumerable<RulesetFacilityRuleChange> rules);
+
         void SetActiveRulesetId(int rulesetId);
         Task<Ruleset> SetCustomDefaultRulesetAsync(int rulesetId);
-        Task<bool> ExportRulesetToJsonFile(int rulesetId, CancellationToken cancellationToken);
-        Task<Ruleset> ImportNewRulesetFromJsonFile(string fileName, bool returnCollections = false, bool returnOverlayConfiguration = false);
-        IEnumerable<string> GetJsonRulesetFileNames();
+
         Task<bool> CanDeleteRuleset(int rulesetId, CancellationToken cancellationToken);
         Task<bool> HasRulesetBeenUsedAsync(int rulesetId, CancellationToken cancellationToken);
         Task<bool> DeleteRulesetAsync(int rulesetId);
-        Task<IEnumerable<RulesetItemRule>> GetRulesetItemRulesAsync(int rulesetId, CancellationToken cancellationToken);
-        Task SaveRulesetItemRules(int rulesetId, IEnumerable<RulesetItemRule> rules);
-        Task<IEnumerable<RulesetItemCategoryRule>> GetRulesetItemCategoryRulesDeferringToItemRules(int rulesetId, CancellationToken cancellationToken);
-        Task<IEnumerable<ItemCategory>> GetItemCategoriesDeferringToItemRules(int rulesetId, CancellationToken cancellationToken);
-        Task<RulesetOverlayConfiguration> GetRulesetOverlayConfigurationAsync(int rulesetId, CancellationToken cancellationToken);
+        
+        Task<bool> ExportRulesetToJsonFile(int rulesetId, CancellationToken cancellationToken);
+        Task<Ruleset> ImportNewRulesetFromJsonFile(string fileName, bool returnCollections = false, bool returnOverlayConfiguration = false);
+        IEnumerable<string> GetJsonRulesetFileNames();
     }
 }
