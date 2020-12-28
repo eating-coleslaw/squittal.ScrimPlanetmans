b49194ca18663f6ab5df6439c138d15d00921f1a
Chirtle <10554267+chirtle@users.noreply.github.com>
2020-12-28 14:51:56 -0600

    add

---
+++ squittal.ScrimPlanetmans.App/Data/SQL/MigrationHelpers/Backfill_RulesetOverlayConfiguration.sql
@@ -0,0 +1,26 @@
+USE [PlanetmansDbContext]
+
+DECLARE @nCursorRulesetId INT;
+
+DECLARE RULESET_CURSOR CURSOR FAST_FORWARD FOR
+  SELECT ruleset.Id
+    FROM Ruleset ruleset
+      LEFT OUTER JOIN RulesetOverlayConfiguration overlay
+        ON ruleset.Id = overlay.RulesetId
+    WHERE overlay.RulesetId IS NULL
+
+OPEN RULESET_CURSOR
+FETCH NEXT FROM RULESET_CURSOR INTO @nCursorRulesetId
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+
+  INSERT INTO RulesetOverlayConfiguration ( RulesetId, UseCompactLayout, StatsDisplayType, ShowStatusPanelScores )
+    VALUES ( @nCursorRulesetId, 0, 1, null );
+
+  FETCH NEXT FROM RULESET_CURSOR INTO @nCursorRulesetId;
+
+END;
+
+CLOSE RULESET_CURSOR;
+DEALLOCATE RULESET_CURSOR;
