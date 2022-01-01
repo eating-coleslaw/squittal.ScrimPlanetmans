aeca7ef9679599270a0b706e2eeb25b5ceeb8f3c
Chirtle <10554267+eating-coleslaw@users.noreply.github.com>
2022-01-01 14:56:26 -0600

    Don't set default values for nullable types

--- squittal.ScrimPlanetmans.App/Data/DataConfigurations/ScrimMatch/RulesetConfiguration.cs
+++ squittal.ScrimPlanetmans.App/Data/DataConfigurations/ScrimMatch/RulesetConfiguration.cs
@@ -17,25 +17,23 @@ namespace squittal.ScrimPlanetmans.Data.DataConfigurations
             builder.Property(e => e.IsCustomDefault).HasDefaultValue(false);
             builder.Property(e => e.IsDefault).HasDefaultValue(false);
 
-            builder.Property(e => e.EnableRoundTimeLimit).HasDefaultValue(true);
+            //builder.Property(e => e.EnableRoundTimeLimit).HasDefaultValue(true);
             builder.Property(e => e.DefaultRoundLength).HasDefaultValue(900);
-            builder.Property(e => e.RoundTimerDirection).HasDefaultValue(null);
+            //builder.Property(e => e.RoundTimerDirection).HasDefaultValue(null);
 
             builder.Property(e => e.DefaultEndRoundOnFacilityCapture).HasDefaultValue(false);
      
             builder.Property(e => e.EndRoundOnPointValueReached).HasDefaultValue(false);
-            builder.Property(e => e.TargetPointValue).HasDefaultValue(null);
-            builder.Property(e => e.InitialPoints).HasDefaultValue(null);
+            //builder.Property(e => e.TargetPointValue).HasDefaultValue(null);
+            //builder.Property(e => e.InitialPoints).HasDefaultValue(null);
             
             builder.Property(e => e.MatchWinCondition).HasDefaultValue(MatchWinCondition.MostPoints);
             builder.Property(e => e.RoundWinCondition).HasDefaultValue(RoundWinCondition.NotApplicable);
             
             builder.Property(e => e.EnablePeriodicFacilityControlRewards).HasDefaultValue(false);
-            builder.Property(e => e.PeriodicFacilityControlPoints).HasDefaultValue(null);
-            builder.Property(e => e.PeriodicFacilityControlInterval).HasDefaultValue(null);
-            builder.Property(e => e.PeriodFacilityControlPointAttributionType).HasDefaultValue(null);
-
-            
+            //builder.Property(e => e.PeriodicFacilityControlPoints).HasDefaultValue(null);
+            //builder.Property(e => e.PeriodicFacilityControlInterval).HasDefaultValue(null);
+            //builder.Property(e => e.PeriodFacilityControlPointAttributionType).HasDefaultValue((PointAttributionType?)null);
         }
     }
 }
