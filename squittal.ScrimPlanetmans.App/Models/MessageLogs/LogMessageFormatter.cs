0fafd6b2fd6fba2069023cce6e759b0aec1413a2
Chirtle <10554267+eating-coleslaw@users.noreply.github.com>
2022-11-27 11:04:11 -0600

    Add util classes

---
+++ squittal.ScrimPlanetmans.App/Models/MessageLogs/LogMessageFormatter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace squittal.ScrimPlanetmans.Models.MessageLogs
+{
+    public static class LogMessageFormatter
+    {
+        public static string Error(string header, string body)
+        {
+            return $"<span style=\"color: red;\"><span style=\"font-weight: 600;\">{header}</span>. {body}</span>";
+        }
+    }
+}
