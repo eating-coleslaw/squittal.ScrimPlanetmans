518578f4f06f608fa7cc8e35185512b9e00a053b
Chirtle <10554267+eating-coleslaw@users.noreply.github.com>
2023-02-04 10:50:17 -0600

    Add combined player/outfit addition form to Team Composer

---
+++ squittal.ScrimPlanetmans.App/Models/Forms/TeamAdditionEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace squittal.ScrimPlanetmans.Models.Forms
+{
+    public class TeamAdditionEventArgs : EventArgs
+    {
+        public string AdditionIdentifier { get; set; }
+        public TeamAdditionInputType AdditionType { get; set; }
+
+        public TeamAdditionEventArgs(TeamAdditionInputType additionType)
+        {
+            AdditionType = additionType;
+        }
+
+        public TeamAdditionEventArgs(string additionIdentifier, TeamAdditionInputType additionType)
+        {
+            AdditionIdentifier = additionIdentifier;
+            AdditionType = additionType;
+        }
+    }
+}
