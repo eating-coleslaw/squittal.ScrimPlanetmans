3c272231faa919e21d0597ea676b331cd4355824
Chirtle <10554267+eating-coleslaw@users.noreply.github.com>
2023-04-01 17:49:02 -0500

    Acctually store FacilityId in ScrimFacilityControl model to DB

--- squittal.ScrimPlanetmans.App/CensusStream/WebsocketEventHandler.cs
+++ squittal.ScrimPlanetmans.App/CensusStream/WebsocketEventHandler.cs
@@ -1380,6 +1380,7 @@ namespace squittal.ScrimPlanetmans.CensusStream
                         ControlType = controlEvent.ControlType,
                         ControllingFactionId = (int)controllingFaction,
                         ZoneId = controlEvent.ZoneId != null ? controlEvent.ZoneId : (int?)null,
+                        FacilityId = payload.FacilityId,
                         WorldId = payload.WorldId,
                         Points = controlEvent.Points,
                     };
